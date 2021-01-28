3825b4d9d0a59abf6db925af32898b654d5ad47d
GJINXIN <1782561167@qq.com>
2021-01-28 10:17:59 +0800

    页面跳转controller

---
+++ src/main/java/com/score/scorestatistics/controller/PageController.java
@@ -0,0 +1,14 @@
+package com.score.scorestatistics.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/*页面跳转Controler */
+@Controller
+public class PageController {
+    @RequestMapping("/{page}")
+    public String showPage(@PathVariable String page){
+        return page;
+    }
+}
