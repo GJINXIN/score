52d3b194b44f0bf757ce8061a0746fe7bd1c86dc
GJINXIN <1782561167@qq.com>
2021-01-29 21:36:34 +0800

    lombok修复

--- src/main/java/com/score/scorestatistics/pojo/Score.java
+++ src/main/java/com/score/scorestatistics/pojo/Score.java
@@ -1,7 +1,16 @@
 package com.score.scorestatistics.pojo;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.util.Date;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Score {
     private Integer id;
 
@@ -13,43 +22,4 @@ public class Score {
 
     private Date tDate;
 
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getSubject() {
-        return subject;
-    }
-
-    public void setSubject(String subject) {
-        this.subject = subject == null ? null : subject.trim();
-    }
-
-    public Float getScore() {
-        return score;
-    }
-
-    public void setScore(Float score) {
-        this.score = score;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type == null ? null : type.trim();
-    }
-
-    public Date gettDate() {
-        return tDate;
-    }
-
-    public void settDate(Date tDate) {
-        this.tDate = tDate;
-    }
 }
